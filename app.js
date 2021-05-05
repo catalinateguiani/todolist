b83845216236abd50339789f33303ad1cb446fcd
Cata001 <catalinateguiani@yahoo.com>
2021-05-05 09:42:32 +0100

    Delete Items from Database

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 mongoose.connect("mongodb://localhost:27017/todolistDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 const itemsSchema = { name: String };
+
 const Item = mongoose.model("Item", itemsSchema);
 
 const item1 = new Item({
@@ -61,9 +63,20 @@ app.post("/", function (req, res) {
   res.redirect("/");
 });
 
-app.get("/work", function (req, res) {
-  res.render("list", { listTitle: "Work List", newListItems: workItems });
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId, function (err) {
+    if (!err) {
+      console.log("Successfully deleted checked item.");
+
+      res.redirect("/");
+    }
+  });
 });
+
+// app.get("/work", function (req, res) {
+//   res.render("list", { listTitle: "Work List", newListItems: workItems });
+// });
 
 app.get("/about", function (req, res) {
   res.render("about");
